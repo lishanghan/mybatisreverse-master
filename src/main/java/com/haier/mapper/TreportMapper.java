fc0bf43ca895085e16579f5c5b47db0402097971
lishaghan <lishaghan@kjtpay.com.cn>
2019-01-16 17:40:12 +0800

    第一次提交

---
+++ src/main/java/com/haier/mapper/TreportMapper.java
@@ -0,0 +1,30 @@
+package com.haier.mapper;
+
+import com.haier.po.Treport;
+import com.haier.po.TreportExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface TreportMapper {
+    long countByExample(TreportExample example);
+
+    int deleteByExample(TreportExample example);
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Treport record);
+
+    int insertSelective(Treport record);
+
+    List<Treport> selectByExample(TreportExample example);
+
+    Treport selectByPrimaryKey(Integer id);
+
+    int updateByExampleSelective(@Param("record") Treport record, @Param("example") TreportExample example);
+
+    int updateByExample(@Param("record") Treport record, @Param("example") TreportExample example);
+
+    int updateByPrimaryKeySelective(Treport record);
+
+    int updateByPrimaryKey(Treport record);
+}
---
+++ src/main/java/sample/GeneratorRun.java
@@ -0,0 +1,36 @@
+package sample;
+
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class GeneratorRun {
+
+    public static void main(String[] args) {
+        try {
+            GeneratorRun generatorRun = new GeneratorRun();
+            generatorRun.generator();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void generator() throws Exception {
+        List<String> warnings = new ArrayList<String>();
+        boolean overwrite = true;
+        //指定 逆向工程配置文件
+        File configFile = new File("./src/main/resources/generatorConfig.xml");
+        ConfigurationParser cp = new ConfigurationParser(warnings);
+        Configuration config = cp.parseConfiguration(configFile);
+        DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+        MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config,
+                callback, warnings);
+        myBatisGenerator.generate(null);
+
+    }
+}
